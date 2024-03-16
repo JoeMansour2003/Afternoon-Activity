1a2b72cc078ffabfab29ea2b22089e9df7f6d67a
Joe Mansour <joemansour2003@gmail.com>
2024-03-16 15:19:46 -0400

    A New Beginning

--- afternoon_activity/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AfternoonActivityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'afternoon_activity'
--- afternoon_activity/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "afternoon_activity"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("cabin/<int:cabin_id>/", views.index, name="detail"),
-]
