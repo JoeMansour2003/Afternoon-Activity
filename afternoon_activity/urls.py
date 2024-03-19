933b66771fae1a9e1814af2926ae1603574f60ba
Joseph Mansour <joemansour2003@gmail.com>
2024-03-19 14:30:11 -0400

    reading campers on cabin page

--- afternoon_activity/urls.py
+++ afternoon_activity/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "afternoon_activity"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("cabin/<int:cabin_id>/", views.index, name="detail"),
+    path("cabin/<int:cabin_id>/", views.index , name="cabin"),
+    # path("cabin/<int:cabin_id>/<string:camper_name>", views.index , name="cabin"),
 ]
