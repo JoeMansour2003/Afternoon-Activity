b50ca9fc29ce9306e51a59aaa9df83533972bd51
Joe Mansour <joemansour2003@gmail.com>
2024-03-26 02:40:39 -0400

    afternoon activites now have juniors seniors and cabin specific activites

--- afternoon_activity/admin.py
+++ afternoon_activity/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Afternoon_Activity,Activity,Camper,Cabin,Counselor,Campers_Afternoon_Relation
+from .models import Afternoon_Activity,Activity,Camper,Cabin,Counselor,Campers_Afternoon_Relation,Group
 
 admin.site.register(Afternoon_Activity)
 admin.site.register(Activity)
@@ -8,3 +8,4 @@ admin.site.register(Camper)
 admin.site.register(Cabin)
 admin.site.register(Counselor)
 admin.site.register(Campers_Afternoon_Relation)
+admin.site.register(Group)
--- afternoon_activity/models.py
+++ afternoon_activity/models.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
-
 
 class Afternoon_Activity(models.Model):
     """
-    This is the Model to print and handout to the counselors, 
+    This is the Model to print and handout to the counselors,
 
     It has all the camper names, the activity and the activity leader as well as the date just to top it off.
     It can be easliy searched by sorting my the second_activity booloean field.
     """
     def __str__(self):
-        return "Afternoon Activity: " + str(self.date) + " " + str(self.activity) + " preference: " + str(self.perference)    
+        return "Afternoon Activity: " + str(self.date) + " " + str(self.activity) + " preference: " + str(self.perference)
     date = models.DateField()
     second_activity= models.BooleanField(default=False)
     activity = models.ForeignKey("Activity", on_delete=models.CASCADE, related_name="activity_with_perference")
@@ -30,6 +32,27 @@ class Campers_Afternoon_Relation(models.Model):
     afternoon_activity = models.ForeignKey("Afternoon_Activity", on_delete=models.CASCADE, related_name="activity_for_camper")
     camper = models.ForeignKey("Camper", on_delete=models.CASCADE, related_name="camper_in_activity")
 
+def get_default_group():
+    return Group.objects.get(group_name="All").id
+
+class Group(models.Model):
+    """
+    Groups for the activities
+    Seniors, Juniors, or specific cabins 1-4 only
+    """
+    UNDELETABLE_NAMES = {"All", "Seniors", "Juniors"}
+
+    def __str__(self):
+        if self.group_name != None:
+            return str(self.group_name)
+        return str(self.group)
+    def delete(self, *args, **kwargs):
+        if self.group_name in self.UNDELETABLE_NAMES:
+            # This is an undeletable group, prevent deletion
+            return
+        super().delete(*args, **kwargs)
+    group_name = models.CharField(max_length=30, null=True, blank=True) # Seniors, Juniors
+    group = models.ManyToManyField("Cabin", related_name="cabins_in_group")
 class Activity(models.Model):
     """
     List of all the activities that the campers can choose from
@@ -37,39 +60,56 @@ class Activity(models.Model):
     def __str__(self):
         return (str(self.activity))
     activity = models.CharField(max_length=20)
+    max_participants = models.IntegerField(default=15)
+    allowed_groups = models.ForeignKey("Group", on_delete=models.CASCADE, related_name="group_for_activity", default=get_default_group())
 
 class Camper(models.Model):
-    """ 
+    """
     Camper profile
     """
     def __str__(self):
         return self.first_name + " " + self.last_name
-    
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     cabin = models.ForeignKey("Cabin", on_delete=models.CASCADE, related_name="cabin_for_camper")
     # afternoon_activity = models.ManyToManyField("Afternoon_Activity", related_name="afternoon_activity_selected", null=True, blank=True)
 
 class Cabin(models.Model):
-    """ 
+    """
     Every Cabin has Campers and Counselors
     """
     def __str__(self):
         return "Cabin: " + str(self.cabin_number)
     cabin_number = models.IntegerField(primary_key=True)
     week_one = models.BooleanField(default=True)
-    # camper = models.ForeignKey("Camper", on_delete=models.CASCADE, related_name="camper_in_cabin", null=True, blank=True) 
+    # camper = models.ForeignKey("Camper", on_delete=models.CASCADE, related_name="camper_in_cabin", null=True, blank=True)
     # counselor = models.ForeignKey("Counselor", on_delete=models.CASCADE, related_name="counselor_in_cabin", null=True, blank=True)
 
+
 class Counselor(models.Model):
-    """ 
+    """
     Counselors only profile
-    
+
     P-staff / volunteers should get there own model"""
     def __str__(self):
         return self.first_name + " " + self.last_name
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, unique=True)
     cabin = models.ForeignKey("Cabin", on_delete=models.CASCADE, related_name="counselors_cabin")
     # possition = models.ForeignKey("Activity", on_delete=models.SET_NULL, related_name="possition_Activity", null=True, default=None, blank=True)
 
+# @receiver(post_save, sender=Counselor)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         group = Group.objects.get(name='Counselor/CIT Staff')[0]
+#         instance.user.groups.add(group)
+#         instance.user.is_staff = True
+#         instance.user.username = instance.email
+#         instance.user.first_name = instance.first_name
+#         instance.user.last_name = instance.last_name
+#         instance.user.email = instance.email
+#         instance.user.set_password('camp2024')
+#         instance.user.save()
