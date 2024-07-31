9a24603a3ae29ac1e52bcbe3b513d0e8330b9ef4
Joseph Mansour <joemansour2003@gmail.com>
2024-07-30 22:49:02 -0400

    fix: allow all host names?

--- camp_t/base_settings.py
+++ camp_t/base_settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # else:
 #     DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost","35.182.190.203", 'ec2-35-182-190-203.ca-central-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ["127.0.0.1","localhost","35.182.190.203", 'ec2-35-182-190-203.ca-central-1.compute.amazonaws.com',"*"]
 
 
 
