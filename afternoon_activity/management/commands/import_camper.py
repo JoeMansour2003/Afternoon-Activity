d6350b1e059a023107d28e3a7b3f00a2cbca9608
Joseph Mansour <joemansour2003@gmail.com>
2024-08-06 00:41:13 -0400

    feat: can now populate the database with camper data from a xlsx file

---
+++ afternoon_activity/management/commands/import_camper.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from django.core.management.base import BaseCommand
+from afternoon_activity.models import Camper, SessionCabin
+
+class Command(BaseCommand):
+    help = 'Import campers from an Excel file'
+
+    def handle(self, *args, **kwargs):
+        # Load the Excel file
+        df = pd.read_excel('/Users/joe/Desktop/Afternoon Activity/camp_t/campers.xlsx')
+
+        # Retrieve the SessionCabin instance for session 1, cabin 1
+        
+
+        # Iterate over the rows of the DataFrame and create Camper objects
+        for index, row in df.iterrows():
+            camper = Camper.objects.create(first_name=row['First_Name'], last_name=row['Last_Name'])
+            session_cabin = SessionCabin.objects.get(session__session_number=row['Session'], cabin__cabin_number=row['Cabin'])
+            camper.session_cabin.add(session_cabin)
+            self.stdout.write('Added camper: ' + str(camper) + ' to ' + str(session_cabin))
+
+
+        self.stdout.write(self.style.SUCCESS('Successfully imported campers'))
