edd449824b1e1b24a6cfaceb70e23e674c520958
Joseph Mansour <joemansour2003@gmail.com>
2025-06-08 21:33:58 -0400

    New stuff

--- camp_t/settings.py
+++ camp_t/settings.py
@@ -1,6 +1,6 @@
 import socket
 print(socket.gethostname())
-if socket.gethostname()=="Joes-MacBook-Air.local":
+if socket.gethostname() in ["Joes-MacBook-Air.local", "Joes-MacBook-Air-2.local"]:
     from camp_t.local_settings import *
 else:
     from camp_t.production_settings import *
